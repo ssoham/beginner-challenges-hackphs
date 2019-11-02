9e37b5a340d12c3908e3c38839c464c750c1bb17
Tax_Day <rohitkundu2014@gmail.com>
2019-11-02 15:42:40 -0400

    three

---
+++ GCD.java
@@ -0,0 +1,14 @@
+public class GCD
+{
+    public void findGCD(int a, int b)
+    {
+        int gcd = -1;
+        for(int i = 1; i <= a && i <= b; i++)
+        {
+            if(a%i==0 && b%i==0)
+                gcd = i;
+        }
+
+        System.out.println(gcd);
+    }
+}
